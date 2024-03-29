1b055cc873f9fbade8ebf1f19c68c5d537a6726d
AfsheenRaza <163439598+AfsheenRaza@users.noreply.github.com>
2024-03-30 00:06:13 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess a number between 1 - 10:",
+    },
+]);
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("Congratulations!you guessed right number.");
+}
+else {
+    console.log("You guessed wrong number.");
+}
